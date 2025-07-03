47962201b2ba6d870c362c291564d5c1ffabb600
saybar <saybar@stud.fra-uas.de>
2025-07-03 23:47:13 +0200

    finish Approach Lights 2

--- Arduino/ApproachLights2.ino
+++ Arduino/ApproachLights2.ino
@@ -5,28 +5,27 @@
 // Matr.Number: 1273441
 // ********************************************************************************
 
-const int ledPins[] = {5, 6, 7, 8, 9};
-const int numLedPins = 5;
+const int ledPins[] = {5, 6, 7, 8};
+const int maxLedPins = 4;
+int numLedPins = 4;
+
 int currentLED = 0;
 
-const int configModeButton = 1;
-const int runModeButton = 2;
+const int configModeButton = 2;
+const int runModeButton = 3;
+const int potentiometerPin = A0;
 
 // Variablen für Modi
 int currentMode = 0; // 0 kein Mode, 1 configMode, 2 runMode
 
-// Button-Entprellung
-bool lastConfigButtonState = HIGH;
-bool lastRunButtonState = HIGH;
-unsigned long lastConfigDebounceTime = 0;
-unsigned long lastRunDebounceTime = 0;
-const unsigned long debounceDelay = 500;
+bool lastConfigModeButtonState = HIGH;
+bool lastRunModeButtonState = HIGH;
 
 void setup() {
   Serial.begin(9600);
   
   // Erst mal alle LED'S initialisieren
-  for (int i = 0; i < numLedPins; i++) {
+  for (int i = 0; i < maxLedPins; i++) {
     pinMode(ledPins[i], OUTPUT);
   }
   
@@ -35,116 +34,78 @@ void setup() {
   pinMode(runModeButton, INPUT_PULLUP);
 
   // Alle LED's ausschalten
-  for (int i = 0; i < numLedPins; i++) {
+  for (int i = 0; i < maxLedPins; i++) {
     digitalWrite(ledPins[i], HIGH);
   }
+
+  // Anfnags maxLedPins initialisieren falls wir als erstes den RunMode laufen lassen
+  int potValue = analogRead(potentiometerPin);
+  numLedPins = map(potValue, 0, 1023, 1, maxLedPins);
+  numLedPins = constrain(numLedPins, 1, maxLedPins);
+  Serial.print("Initiale Anzahl aktiver LEDs (Setup): ");
+  Serial.println(numLedPins);
 }
 
 void loop() {
 
+  bool configModeButtonState = digitalRead(configModeButton);
+  bool runModeButtonState = digitalRead(runModeButton);
   
-  // checkButtons();
-  // checkButtonsTest();
-  checkButtonsNew();
+  if (lastConfigModeButtonState == HIGH && configModeButtonState == LOW) {
+    currentMode = 1;
+    Serial.println("Config Mode aktiviert...");
+  }
+
+  if (lastRunModeButtonState == HIGH && runModeButtonState == LOW) {
+    currentMode = 2;
+    currentLED = 0;
+    // Die LED's die noch aus dem configMode leuchten ausschalten
+    for (int i = 0; i < maxLedPins; i++) {
+      digitalWrite(ledPins[i], HIGH);
+    }
+    Serial.println("Run Mode aktiviert...");
+  }
 
   if (currentMode == 1) {
-    // Config Mode
-    Serial.println("currentMode = 1");
-    
-  } else if (currentMode == 2) {
-    // digitalWrite(ledPins[currentLED], LOW);
-    // delay(2);
-    // digitalWrite(ledPins[currentLED], HIGH);
-    // delay(60);
-    // delayMicroseconds(5);
-    
-    // currentLED++;
-    // if (currentLED >= numLedPins) {
-    //   currentLED = 0;
-    // }
-    Serial.println("currentMode = 2");
-    
-  }
-}
+    Serial.println("Logik für config Mode...");
+    int potValue = analogRead(potentiometerPin);
+    numLedPins = map(potValue, 0, 1023, 1, maxLedPins+1);
+    // numLedPins = constrain(numLedPins, 1, maxLedPins);
 
-void checkButtons() {
+    Serial.print("Potentiometerwert: ");
+    Serial.print(potValue);
+    Serial.print(" => Anzahl aktiver LEDs: ");
+    Serial.println(numLedPins);
 
-  unsigned long currentTime = millis();
-  // Config Button prüfen
-  bool configButtonReadingState = digitalRead(configModeButton);
-  // Wenn der config Button sich eine längere Zeit nicht ändert, laufen wir hier auch nicht rein
-  // und lastConfigDebounceTime liegt immer weiter in der Vergangenheit.
-  // Das müssen wir hier tun, da wir nur auf einen State reagieren wollen, der stabil geblieben ist
-  if (configButtonReadingState != lastConfigButtonState) { 
-    lastConfigDebounceTime = currentTime;
-  }
-  // Wenn die lastConfigDebounceTime stabil geblieben ist (weit genug in der Vergangenheit ist)
-  if ((currentTime - lastConfigDebounceTime) > debounceDelay) {
-    if (configButtonReadingState == LOW && lastConfigButtonState == HIGH) {
-      currentMode = 1;
-      Serial.println("Config Mode aktiviert");
-      // lastRunButtonState = HIGH;
+    for (int i = 0; i < maxLedPins; i++) {
+      if (i < numLedPins) {
+        digitalWrite(ledPins[i], LOW);
+      } else {
+        digitalWrite(ledPins[i], HIGH);
+      }
     }
-    if (configButtonReadingState == HIGH && lastConfigButtonState == LOW) {
-      currentMode = 0;
-      Serial.println("Config Mode deaktiviert");
-    }
+
+    // delay(200);
   }
 
-  lastConfigButtonState = configButtonReadingState;
   
-  // Run Button prüfen
-  bool runButtonReadingState = digitalRead(runModeButton);
-  if (runButtonReadingState != lastRunButtonState) {
-    lastRunDebounceTime = currentTime;
-  }
-  
-  if ((currentTime - lastRunDebounceTime) > debounceDelay) {
-    if (runButtonReadingState == LOW && lastRunButtonState == HIGH) {
-      currentMode = 2;
-      Serial.println("Run Mode aktiviert");
-      // lastConfigButtonState = HIGH;
-    }
-    if (runButtonReadingState == HIGH && lastRunButtonState == LOW) {
-      currentMode = 0;
-      Serial.println("Run Mode deaktiviert");
+  if (currentMode == 2) {
+    Serial.println("Logik für run Mode...");
+    digitalWrite(ledPins[currentLED], LOW);
+    delay(2);
+    digitalWrite(ledPins[currentLED], HIGH);
+    delay(60);
+    delayMicroseconds(5);
+    
+    currentLED++;
+    if (currentLED >= numLedPins) {
+      currentLED = 0;
     }
   }
-  lastRunButtonState = runButtonReadingState;
-}
 
-void checkButtonsTest() {
-  if (digitalRead(configModeButton) == LOW) {  // Direkte Abfrage
-    currentMode = 1;
-    Serial.println("Config Mode (RAW) aktiviert!");
-  }
-  if (digitalRead(runModeButton) == LOW) {
-    currentMode = 2;
-    Serial.println("Run Mode (RAW) aktiviert!");
-  }
-}
+  lastConfigModeButtonState = configModeButtonState;
+  lastRunModeButtonState = runModeButtonState;
 
-void checkButtonsNew() {
-  static unsigned long lastDebounceTime = 0;
-  const unsigned long debounceDelay = 200;  // 50 ms testen
-
-  // Config Button
-  if (digitalRead(configModeButton) == LOW) {
-    if (millis() - lastDebounceTime > debounceDelay) {
-      currentMode = 1;
-      Serial.println("Config Mode aktiviert!");
-    }
-    lastDebounceTime = millis();
-  }
-
-  // Run Button
-  if (digitalRead(runModeButton) == LOW) {
-    if (millis() - lastDebounceTime > debounceDelay) {
-      currentMode = 2;
-      Serial.println("Run Mode aktiviert!");
-    }
-    lastDebounceTime = millis();
-  }
 }
 
 
